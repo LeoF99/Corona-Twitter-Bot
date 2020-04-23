8397f8bf1170c611d2ab1172ce45174679b775f9
Leo <leo.ferreira@dcx.ufpb.br>
2020-04-22 22:47:58 -0300

    Bot base

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'SEe8x7hhvcXvAdPHAq7KmUmXA',
+    consumer_secret:      'mPC7Jp5edZhO0EvU8X0DvxjvB9gmohj0ld3RPKEgaxtodIUwDu',
+    access_token:         '1163491013215903744-g9OQnUn4GZtI9dsPsQjIdlvvf0D1pQ',
+    access_token_secret:  'LKbVJBi0d93PZcE0PtIfAHJYkFsdz8ZB0cG8VXAFeq9uh'
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+const Twit = require('twit');
+const config = require('./config');
+
+const t = new Twit(config);
